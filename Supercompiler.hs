46cbc6db9a704c6fcc2f6b3d9420ca20292a49be
Luigi <gigi@gigi-osx.local>
2016-03-10 21:24:57 +0100

    Added for supercompiler.

---
+++ Supercompiler.hs
@@ -0,0 +1,22 @@
+
+
+type Id = String
+type Value = Int
+
+data Exp = Var Id |
+           Const Int |
+           Plus Exp Exp
+           deriving Show
+
+eval :: Exp -> Value
+eval (Const n) = n
+eval (Plus lhs rhs) = (eval lhs) + (eval rhs)
+
+
+e0 = Const 0
+e1 = Const 1
+e2 = Const 2
+e3 = Plus e0 e1
+e4 = Plus e2 e1
+e5 = Plus e4 e4
+e6 = Plus e5 e4
