459425dda5a493e5474235f84d3f8c52f3ed9030
Luigi <gigi@gigi-osx.local>
2016-03-11 12:32:03 +0100

    Supercompiler draft

--- Supercompiler.hs
+++ Supercompiler.hs
@@ -13,6 +13,8 @@ eval (Const n) = n
 eval (Plus lhs rhs) = (eval lhs) + (eval rhs)
 
 
+
+
 e0 = Const 0
 e1 = Const 1
 e2 = Const 2
@@ -20,3 +22,8 @@ e3 = Plus e0 e1
 e4 = Plus e2 e1
 e5 = Plus e4 e4
 e6 = Plus e5 e4
+
+
+main = do
+  print "asdfasdf"
+  print "asdfasdf"
