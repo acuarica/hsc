853620fed52acc58126d7ed4506c322412603931
Luigi <gigi@gigi-osx.local>
2016-04-06 09:48:15 +0200

    Progress

--- Algebra.hs
+++ Algebra.hs
@@ -17,11 +17,24 @@ showfl :: Fix [] -> String
 showfl (Fx []) = "Fx-[]"
 showfl (Fx (x:xs)) = "Fx-(" ++ showfl x ++ ":" ++ showfl (Fx xs) ++ ")"  
 
-instance Show (Fix Maybe) where
-  show = showfm
+class ShowF f where
+  showF :: Show a => f a -> String
 
-instance Show (Fix []) where
-  show = showfl
+instance ShowF f => Show (f (Fix f)) where
+  show = showF
+
+instance ShowF f => Show (Fix f) where
+  show (Fx a) = "Fx" ++ show a
+
+--instance ShowF (Maybe) where
+ -- showF = show 
+--instance Show
+
+--instance Show (Fix Maybe) where
+  --show = showfm
+
+--instance Show (Fix []) where
+  --show = showfl
 
 a, b, c :: Fix Maybe
 a = Fx Nothing 
@@ -36,12 +49,12 @@ g = Fx [Fx [Fx [Fx [Fx [Fx []]]]]]
 
 main :: IO ()
 main = do
-  print a
-  print b
-  print c
-  print d
-  print e
-  print f
-  print g
+  putStrLn $ show a
+  putStrLn $ showfm b
+  putStrLn $ showfm c
+  putStrLn $ showfl d
+  putStrLn $ showfl e
+  putStrLn $ showfl f
+  putStrLn $ showfl g
   return ()
 
