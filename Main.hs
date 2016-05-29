361a455d6a45deba2869e232f4d4c321b2100b58
Luis Mastrangelo <luis.mastrangelo@usi.ch>
2016-05-30 01:45:07 +0200

    Added dot

--- Main.hs
+++ Main.hs
@@ -4,11 +4,24 @@ module Main where
 import System.Exit (exitFailure)
 import System.Environment (getArgs)
 import Language.Haskell.Exts (parseFileContents, fromParseResult)
+import Data.List
 
+import Eval
 import HSE
 
 usage :: String
 usage = "Usage: hsc <haskell file.hs>"
+
+dot :: Conf -> [Conf]
+dot conf = conf:(case step conf of
+  Nothing -> []
+  Just conf' -> dot conf')
+
+dot' :: [Conf] -> String
+dot' cs = "digraph G {" ++ tolist cs ++ "}"
+  where
+    tolist cs = intercalate "->" (map tostr cs)
+    tostr conf = "\"" ++ show (toExpr conf) ++ "\""
 
 main :: IO ()
 main = do
@@ -22,5 +35,10 @@ main = do
       putStrLn $ "Processing " ++ fileName
       fileText <- readFile fileName
       let hse = fromParseResult (parseFileContents fileText)
-      print $ fromHSE hse
+      let expr = fromHSE hse
+      --print $ expr
+      --print $ (dot . newConf emptyEnv) expr
+      let res = dot' $ (dot . newConf emptyEnv) expr
+      putStrLn $ res
+      writeFile "hola.dot" res
       return ()
