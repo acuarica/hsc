892937bdaa2e0162c90b8fb5842913c63d98782f
gigi <gigi@gigi-osx.local>
2016-09-16 19:35:43 +0200

    More on dot

--- Main.hs
+++ Main.hs
@@ -18,56 +18,39 @@ import HSE (fromHSE)
 usage :: String
 usage = "Usage: hsc <haskell-file.hs | expr-file.expr>"
 
-dot :: Conf -> [Conf]
-dot conf = conf:(case step conf of
-  Nothing -> []
-  Just conf' -> dot conf')
-
-dot' :: [Conf] -> String
-dot' cs = "digraph G {" ++ tolist cs ++ "}"
-  where
-    tolist cs = intercalate "->" (map tostr cs)
-    tostr conf = "\"" ++ show (toExpr conf) ++ "\""
-
 fromFileName :: FilePath -> String -> FilePath
 fromFileName fileName ext = fileName ++ "." ++ ext
 
-dotFromHist :: [HistEdge] -> String
-dotFromHist [] = ""
-dotFromHist ((parentVar, label, var, fv, conf):es) =
-  printf "\t\"%s\":\"%s\" -> \"%s\" [label=\"%s\"]\n"
-    parentVar (show label) var (show label) ++
-  --"\t\"" ++ parentVar ++ "\" -> \"" ++ var ++ "\"[label=\""++show label++"\"]\n" ++
-  dotFromHist es
+-- \\tgraph [ordering=out]\n\
 
-dotFromHist2 :: Var -> [HistNode] -> String
-dotFromHist2 var0 [] = ""
-dotFromHist2 var0 ((var, fvs, node, (env, stack, expr), sps):vs) =
-  printf "\t\"%s\" [shape=record,label=\"{{<p0>%s|%s}|%s%s}\"%s]\n"
-    var var (unwords fvs) (pprint node) ports style ++
-  --"\t\"" ++ var ++       "\"[label=\"" ++ show expr ++ "\"]\n" ++
-  dotFromHist2 var0 vs
-  where
-    style = if var == var0 then ",style=\"rounded,bold\"" else ""
-    pprint VarNode = show expr
-    pprint ArgNode = show expr ++ show stack
-    pprint ConNode = let (Con t vs) = expr in t
-    pprint CaseNode = "case " ++ show expr ++ " of"--let (Con t vs) = expr in t
-    port = printf "<%s>%s"
-    ports = case node of
-      VarNode -> ""
-      ArgNode -> ""
-      ConNode -> "|{" ++ let (Con tag vs) = expr in intercalate "|" (zipWith (\v i -> port (tag ++"_"++show i) (show v)) vs [1..length vs] ) ++"}"
-      CaseNode -> "|{" ++ intercalate "|" (map (\l-> port (show l) (show l)) (fst (unzip sps))) ++ "}"
-
-  -- \\trankdir=LR\n\
-wrapDot :: String -> String
-wrapDot = printf
+makeDot :: Var -> Hist -> String
+makeDot var0 (es, vs) = printf
   "digraph G {\n\
-  \\tnode [style=rounded, fontname=\"Monaco\", fontsize=12]\n\
+  \\tnode [shape=record, style=rounded, fontname=\"Monaco\", fontsize=12]\n\
   \\tedge [fontname=\"Monaco\", fontsize=12]\n\
+  \\t\"%s\" [style=\"rounded, bold\"]\n\
+  \%s\n\
   \%s\
   \}\n"
+  var0 (foldr ((++) . dotEdge) "" es) (foldr ((++) . dotNode) "" vs)
+  where
+    dotEdge (parentVar, label, var, fv, conf) =
+      printf "\t\"%s\":\"%s\" -> \"%s\"\n"
+        parentVar (show label) var
+    dotNode (var, fvs, node, (env, stack, expr), sps) =
+      let port = printf "<%s>%s" in
+      let (pnode, ports) = case node of
+            VarNode -> (show expr, "")
+            ArgNode -> (show expr ++ show stack, "")
+            ConNode ->
+              (let (Con t vs) = expr in t,
+              "|{" ++
+              let (Con tag vs) = expr in
+              intercalate "|"
+                (zipWith (\v i -> port (tag ++"_"++show i) (show v)) vs [1..length vs] ) ++"}")
+            CaseNode -> ("case " ++ show expr ++ " of", "|{" ++ intercalate "|" (map (\l-> port (show l) (show l)) (fst (unzip sps))) ++ "}") in
+      printf "\t\"%s\" [label=\"{{%s|%s}|%s%s}\"]\n"
+        var var (unwords fvs) pnode ports
 
 filterByExt :: String -> String -> Expr
 filterByExt ext fileText = case lookup ext filters of
@@ -104,14 +87,10 @@ main = do
       putStrLn $ "[Supercompiling " ++ fileName ++ "]"
       fileText <- readFile fileName
       let expr = filterByExt ext fileText
-      let (sexpr, rm@((var0, expr0), ((es, vs), prom))) = supercompileWithMemo expr
-      let dotmm = wrapDot $ dotFromHist es ++ dotFromHist2 var0 (reverse vs)
+      let (sexpr, rm@((var0, expr0), (hist, _))) = supercompileWithMemo expr
 
-      writeFileWithLog (fromFileName fileName "hist") $ show rm
-      writeFileWithLog (fromFileName fileName "sexpr") $ pprint sexpr
-      writeFileWithLog (fromFileName fileName "dot") dotmm
-
-      --let res = dot' $ (dot . newConf emptyEnv) expr
-      --let dotsexpr = dot' $ (dot . newConf emptyEnv) sexpr
+      writeFileWithLog (fromFileName fileName "hist") (show rm)
+      writeFileWithLog (fromFileName fileName "sexpr") (pprint sexpr)
+      writeFileWithLog (fromFileName fileName "dot") (makeDot var0 hist)
 
       return ()
