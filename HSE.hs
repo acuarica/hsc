7c7693724015541fd5eebbd93692b527f5690e5d
luigi <luismastrangelo@usi.ch>
2016-11-24 22:29:01 +0100

    Working with new version of Haskell.Language.Exts

--- HSE.hs
+++ HSE.hs
@@ -6,67 +6,77 @@ import qualified Language.Haskell.Exts as H
 
 import Expr (Expr(Var, Con, Lam, App, Case), Var, Pat(Pat), con, let1, zero)
 
-fromHSE :: Expr -> Module -> Expr
-fromHSE rootExpr (Module _ _ _ _ _ _ decls) =
+fromHSE :: Expr -> Module l -> Expr
+fromHSE rootExpr (Module _ _ _ _ decls) =
   foldr foldExpr rootExpr decls
   where foldExpr decl expr = case fromDecl decl of
           Nothing -> expr
           Just (v, d) -> let1 v d expr
 
-fromDecl :: Decl -> Maybe (Var, Expr)
-fromDecl (PatBind _ (PVar f) (UnGuardedRhs x) (Just (BDecls []))) =
+fromDecl :: Decl l -> Maybe (Var, Expr)
+fromDecl (PatBind _ (PVar _ f) (UnGuardedRhs _ x) (Just (BDecls _ []))) =
   Just (fromName f, fromExp x)
-fromDecl (PatBind _ (PVar f) (UnGuardedRhs x) Nothing) =
+fromDecl (PatBind _ (PVar _ f) (UnGuardedRhs _ x) Nothing) =
   Just (fromName f, fromExp x)
-fromDecl (FunBind [Match _ f vars Nothing (UnGuardedRhs x) (Just (BDecls []))]) =
-  Just (fromName f, fromExp $ Lambda sl vars x)
-fromDecl (FunBind [Match _ f vars Nothing (UnGuardedRhs x) Nothing]) =
-  Just (fromName f, fromExp $ Lambda sl vars x)
+fromDecl (FunBind l [Match _ f vars (UnGuardedRhs _ x) (Just (BDecls _ []))]) =
+  Just (fromName f, fromExp $ Lambda l vars x)
+fromDecl (FunBind l [Match _ f vars (UnGuardedRhs _ x) Nothing]) =
+  Just (fromName f, fromExp $ Lambda l vars x)
 
-fromDecl d@(FunBind ms) = unhandle "fromDecl:test" d
+--fromDecl d@(FunBind _ ms) = unhandle "fromDecl:test" d
+
 --  Just ("$$", Lam "$scvar" (Case (Var "$scvar") (map fromMatch ms)))
 
 fromDecl TypeSig{} = Nothing
 fromDecl DataDecl{} = Nothing
 fromDecl TypeDecl{} = Nothing
 fromDecl RulePragmaDecl{} = Nothing
-fromDecl decl = unhandle "fromDecl" decl
+--fromDecl decl = unhandle "fromDecl" decl
 
-fromMatch (Match _ f [pat] Nothing (UnGuardedRhs x) Nothing) =
+fromMatch (Match _ f [pat] (UnGuardedRhs _ x) Nothing) =
   (fromPat pat, fromExp x)
-fromMatch (Match _ f [pat] Nothing (UnGuardedRhs x) (Just (BDecls []))) =
+fromMatch (Match _ f [pat] (UnGuardedRhs _ x) (Just (BDecls _ []))) =
   (fromPat pat, fromExp x)
 fromMatch m = unhandle "fromMatch" m
 
-fromExp :: Exp -> Expr
+fromExp :: Exp l -> Expr
 fromExp (Lambda _ [] x) = fromExp x
-fromExp (Lambda _ (PVar (Ident x):vars) bod) = Lam x $ fromExp $ Lambda sl vars bod
-fromExp (H.App x y) = App (fromExp x) (fromExp y)
-fromExp (H.Var (UnQual name)) = Var (fromName name)
-fromExp (H.Con (UnQual name)) = con (fromName name)
-fromExp (H.Con (Special Cons)) = con "Cons"
+fromExp (Lambda l (PVar _ (Ident _ x):vars) bod) =
+  Lam x $ fromExp $ Lambda l vars bod
+fromExp (H.App _ x y) = App (fromExp x) (fromExp y)
+fromExp (H.Var _ (UnQual _ name)) = Var (fromName name)
+fromExp (H.Con _ (UnQual _ name)) = con (fromName name)
+fromExp (H.Con _ (Special _ (Cons _))) = con "Cons"
 -- fromExp (LeftSection x (QVarOp y)) = fromExp $ H.App (H.Var y) x
-fromExp (Paren x) = fromExp x
-fromExp (H.Case sc alts) = Case (fromExp sc) (map fromAlt alts)
-fromExp (List []) = con "Nil"
-fromExp (List (x:xs)) = fromExp $ InfixApp x (QConOp (Special Cons)) $ List xs
+fromExp (Paren _ x) = fromExp x
+fromExp (H.Case _ sc alts) = Case (fromExp sc) (map fromAlt alts)
+fromExp (List _ []) = con "Nil"
+fromExp (List l (x:xs)) =
+  fromExp $ InfixApp l x (QConOp l (Special l (Cons l))) $ List l xs
 
---fromExp (List (x:xs)) = fromExp $ InfixApp x (QConOp (Special Cons)) $ List []
+--fromExp (List (x:xs)) =
+    --fromExp $ InfixApp x (QConOp (Special Cons)) $ List []
 
-fromExp (InfixApp x (QConOp (Special Cons)) y) = Con "Cons" [fromExp x, fromExp y]
-fromExp (InfixApp a (QVarOp b) c) = fromExp $ H.App (H.App (H.Var b) a) c
-fromExp (Lit (Int n)) | n >= 0 = f n
+fromExp (InfixApp _ x (QConOp _ (Special _ (Cons _))) y) =
+  Con "Cons" [fromExp x, fromExp y]
+fromExp (InfixApp l a (QVarOp _ b) c) =
+  fromExp $ H.App l (H.App l (H.Var l b) a) c
+fromExp (Lit _ (Int _ n _)) | n >= 0 = f n
   where f n = if n == 0 then zero else Con "Succ" [f (n-1)]
-fromExp (Lit (Char c)) = con $ "$C" ++ [c]
-fromExp (Lit (String "")) = con "Nil"
-fromExp (Lit (String (x:xs))) =
-  Con "Cons" [fromExp (Lit (Char x)), fromExp (Lit (String xs))]
+fromExp (Lit _ (Char _ c _)) = con $ "$C" ++ [c]
+fromExp (Lit _ (String _ "" _)) = con "Nil"
+fromExp (Lit l (String _ (x:xs) _)) =
+  Con "Cons" [fromExp (Lit l (Char l x "?")),
+    fromExp (Lit l (String l xs "?"))]
 
 --fromExp (Lit x) = Con noname (prettyPrint x) []
 -- fromExp x@(NegApp _) = Con noname (prettyPrint x) []
 -- fromExp (If a b c) = fromExp $ H.Case a [f "True" b, f "False" c]
---     where f con x = Alt sl (PApp (UnQual $ Ident con) []) (UnGuardedRhs x) (Just (BDecls []))
--- fromExp o@(H.Let (BDecls xs) x) = Let noname ((f1,fromExp x):concatMap fromDecl xs) f1
+--     where f con x =
+    --Alt sl (PApp (UnQual $ Ident con) [])
+    --  (UnGuardedRhs x) (Just (BDecls []))
+-- fromExp o@(H.Let (BDecls xs) x) =
+   --Let noname ((f1,fromExp x):concatMap fromDecl xs) f1
 --     where f1:_ = freshNames o
 -- fromExp o@(Tuple _ xs) =
 --   Let noname
@@ -77,40 +87,41 @@ fromExp (Lit (String (x:xs))) =
 --   Con noname (fromTuple $ replicate n ()) []
 -- fromExp e@(Do stmts) =
 --   error ("fromExp Do: " ++ prettyPrint e ++ "\n --or--\n" ++ show e)
-fromExp e = unhandle "fromExp" e
+--fromExp e = unhandle "fromExp" e
 
-fromName :: H.Name -> Var
-fromName (Ident var) = var
-fromName (Symbol var) = var
+fromName :: H.Name l -> Var
+fromName (Ident _ var) = var
+fromName (Symbol _ var) = var
 
-fromAlt :: Alt -> (Pat, Expr)
-fromAlt (Alt _ pat (UnGuardedRhs bod) (Just (BDecls []))) =
+fromAlt :: Alt l -> (Pat, Expr)
+fromAlt (Alt _ pat (UnGuardedRhs _ bod) (Just (BDecls _ []))) =
   (fromPat pat, fromExp bod)
-fromAlt (Alt _ pat (UnGuardedRhs bod) Nothing) =
+fromAlt (Alt _ pat (UnGuardedRhs _ bod) Nothing) =
   (fromPat pat, fromExp bod)
-fromAlt x = error $ "Unhandled fromAlt: " ++ show x
+--fromAlt x = error $ "Unhandled fromAlt: " ++ show x
 
-fromPat :: H.Pat -> Pat
-fromPat (PParen x) = fromPat x
-fromPat (PList []) = Pat "Nil" []
-fromPat (PApp (Special Cons) xs) = Pat "Cons" $ map fromPatVar xs
-fromPat (PInfixApp a b c) = fromPat $ PApp b [a,c]
-fromPat (PApp (UnQual c) xs) = Pat (fromName c) $ map fromPatVar xs
-fromPat (PTuple _ xs) = Pat (fromTuple xs) $ map fromPatVar xs
-fromPat (PApp (Special (TupleCon _ n)) xs) = Pat (fromTuple xs) $
+fromPat :: H.Pat l -> Pat
+fromPat (PParen _ x) = fromPat x
+fromPat (PList _ []) = Pat "Nil" []
+fromPat (PApp _ (Special _ (Cons _ )) xs) = Pat "Cons" $ map fromPatVar xs
+fromPat (PInfixApp l a b c) = fromPat $ PApp l b [a,c]
+fromPat (PApp _ (UnQual _ c) xs) = Pat (fromName c) $ map fromPatVar xs
+fromPat (PTuple _ _ xs) = Pat (fromTuple xs) $ map fromPatVar xs
+fromPat (PApp _ (Special _ (TupleCon _ _ n)) xs) = Pat (fromTuple xs) $
     map fromPatVar xs
-fromPat PWildCard = Pat "_wild" []
-fromPat x = unhandle "fromPat" x
+fromPat (PWildCard _) = Pat "_wild" []
+--fromPat x = unhandle "fromPat" x
 
 fromTuple xs = "(" ++ replicate (length xs - 1) ',' ++ ")"
 
-fromPatVar :: H.Pat -> Var
-fromPatVar (PVar x) = fromName x
-fromPatVar PWildCard = "$__new__"
-fromPatVar x = unhandle "fromPatVar" x
+fromPatVar :: H.Pat l -> Var
+fromPatVar (PVar _ x) = fromName x
+fromPatVar (PWildCard _) = "$__new__"
+--fromPatVar x = unhandle "fromPatVar" x
 
-sl :: SrcLoc
-sl = SrcLoc "" 0 0
+--replaced by l
+--sl :: SrcLoc
+--sl = SrcLoc "" 0 0
 
 unhandle :: (Show a, Pretty a) => String -> a -> t
 unhandle w e = error $ "Unhandled HSE." ++ w ++ ": \n" ++
